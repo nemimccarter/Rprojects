68155d020fec07f0dd612a2aba4bc3ca2a161dc2
nemimccarter <nemimccarter@gmail.com>
2017-02-23 17:42:29 -0800

    started hw2git add . added parks.csv and .pdf of proposal for finalproject

--- hw2/hw2.R
+++ hw2/hw2.R
@@ -1,5 +1,4 @@
-1. Describe the null hypotheses to which the p-values given in Table 3.4
-correspond. Explain what conclusions you can draw based on these
-p-values. Your explanation should be phrased in terms of sales, TV,
-radio, and newspaper, rather than in terms of the coefficients of the
-linear model.
+library(ISLR)
+Carseats
+lm.fit = lm(Carseats$Sales~Carseats$Price+Carseats$Urban+Carseats$US)
+lm.fit
