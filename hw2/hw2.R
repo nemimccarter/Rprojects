5123821c29ca6692724fe4a30b36d12f65fbd74f
nemimccarter <nemimccarter@gmail.com>
2017-02-27 20:28:11 -0800

    submitting

--- hw2/hw2.R
+++ hw2/hw2.R
@@ -1,1 +1,20 @@
 library(ISLR)
+
+ls.fit = lm(Y~x)
+x = rnorm(100, mean = 0, sd = 1)
+eps = rnorm(100, mean = 0, sd = sqrt(0.25 / 10))
+Y = -1 + (0.5 * x) + eps
+# (d) create a scatterplot, comment on observations
+plot(x, Y)
+ls.fitLessNoise = lm(Y~x)
+summary(ls.fitLessNoise)
+
+x = rnorm(100, mean = 0, sd = 1)
+eps = rnorm(100, mean = 0, sd = sqrt(0.25))
+lm.fit = lm(Y~x + x^2)
+lm.fit = lm(Y~x)
+lm.fit
+summary(lm.fit)
+Residual standard error: 0.5263 on 98 degrees of freedom
+Multiple R-squared:  0.000384,	Adjusted R-squared:  -0.009816 
+F-statistic: 0.03765 on 1 and 98 DF,  p-value: 0.8465
