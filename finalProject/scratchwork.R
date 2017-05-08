9600bad8430c23933643811a954fafba1aa6686c
nemimccarter <nemimccarter@gmail.com>
2017-05-07 19:44:51 -0700

    Add fungi study to markdown. Remove fungi study from scratch

--- finalProject/scratchwork.R
+++ finalProject/scratchwork.R
@@ -21,29 +21,3 @@ kmeans.out$cluster
 plot(x, col=(kmeans.out$cluster +1), main="Pop vs. Lazarus", xlab="", ylab="", pch=20, cex=2)
 
 ## Prep data ##
-
-#fungi
-park.fungi = subset(species, Category == "Bird")
-species.fungi = subset(species.fungi.middle, Occurrence == "Present")
-park.fungi = data.frame(table(species.fungi$Park.Name))
-rownames(park.fungi) <- park.fungi$Var1
-park.fungi = subset(park.fungi, select = -Var1)
-
-#latitude
-parks.lat = subset(park.names, select = c(Park.Name, Latitude))
-
-park.fungi = data.frame(park.fungi, parks.lat$Latitude)
-# renaming
-colnames(park.fungi)[colnames(park.fungi)=="parks.lat.Latitude"] <- "Latitude"
-colnames(park.fungi)[colnames(park.fungi)=="Freq"] <- "Fungi.Freq"
-
-## Begin k-means with 2 clusters ##
-set.seed(1)
-x = data.frame(park.fungi$Fungi.Freq, park.fungi$Latitude)
-View(x)
-kmeans(x,2,nstart=20)
-kmeans.out = kmeans(x,2,nstart=20)
-plot(kmeans.out)
-kmeans.out$cluster
-plot(kmeans.out$cluster)
-kmeans.out$centers
